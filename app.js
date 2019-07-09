6053acd8921bbecfde415b9dd0f4fba493f9b145
Madhur Kapoor <madhur.kapoor@open.ac.uk>
2019-07-09 07:13:28 +0530

    Added Get method to return data from mongoDb

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const db = mongoose.connect('mongodb://localhost/bookAPI');
+const Book = require('./models/bookModel');
 
 const port = process.env.PORT || 3000;
 
 const app = express();
 
+
+
 const bookRouter = express.Router();
 bookRouter.route('/books')
-.get((req, res) => {
-  var sample = {hello: 'Hello from Router'};
-  res.json(sample);
-});
+  .get((req, res) => {
+    const query = {};
+    if(req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    Book.find(query,(err, books) => {
+      if(err) {
+        res.send(err);
+      } else {
+        res.json(books);
+      }
+    })
+  });
 
 app.use('/api', bookRouter)
 
---
+++ models/bookModel.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const bookModel = new Schema({
+  title: {
+    type: String
+  },
+  author: {
+    type: String
+  },
+  genre: {
+    type: String
+  },
+  read: {
+    type: String,
+    default: false
+  }
+
+});
+
+module.exports = mongoose.model('Book', bookModel);
